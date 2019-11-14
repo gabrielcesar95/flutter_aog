75abe932045cbb5522a2859ade9240bb414794d5
Gabriel Cesar <gabriel>
2019-11-14 17:40:35 -0300

    changed app icon

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -20,7 +20,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       backgroundColor: Theme.of(context).primaryColor,
       body: AnimatedContainer(
         duration: Duration(
